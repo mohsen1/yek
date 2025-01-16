60011ac494626afc484e67a00afcd46b4e226f01
Mohsen Azimi <me@azimi.me>
2025-01-16 15:36:09 +0700

    Remove unused core formula

--- yek.core.rb
+++
@@ -1,9 +0,0 @@
-class Yek < Formula
-  desc "Repository content chunker and serializer for LLM consumption"
-  homepage "https://github.com/mohsen1/yek"
-  deprecate! date: "2024-01-13", because: "has been moved to mohsen1/yek/yek"
-
-  def install
-    odie "Please use 'brew install mohsen1/yek/yek' instead"
-  end
-end 
