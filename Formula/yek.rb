c05f6e74823bc894b843732571e959d55e94222e
Mohsen Azimi <me@azimi.me>
2025-01-19 17:06:41 +0700

    release: v0.13.1

--- Formula/yek.rb
+++ Formula/yek.rb
@@ -1,13 +1,13 @@
 class Yek < Formula
   desc "A tool to chunk and serialize repository content for LLM consumption"
   homepage "https://github.com/bodo-run/yek"
-  version "0.7.5"
+  version "0.13.1"
   head "https://github.com/bodo-run/yek.git", branch: "main"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/bodo-run/yek/releases/download/v#{version}/yek-aarch64-apple-darwin.tar.gz"
-      sha256 "788f8f3e6d4121f7ca8ab9586d1f1e7e04bd0b3930ab7571465ac1baa38fc626"  # arm64
+      sha256 "110971f28072cb5276e5fe5b5cefa9c35057233cf294662fbf9ee77ee2968b70"  # arm64
     else
       url "https://github.com/bodo-run/yek/releases/download/v#{version}/yek-x86_64-apple-darwin.tar.gz"
       sha256 "34896ad65e8ae7c5e93d90e87f15656b67ed5b7596492863d1da80e548ba7301"  # x86_64
